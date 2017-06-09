37f3f783c6753a8a1a5e1d68fc06097e4a2bc36c
BenPorat <shahaf@wix.com>
2017-06-09 13:03:11 +0300

    test

---
+++ personsArr.js
@@ -0,0 +1,304 @@
+let personArr = [
+    {
+        "_id": "593a689ebe10b4869442b609",
+        "index": 0,
+        "name": "Olsen",
+        "surename": "Hayden",
+        "married": true,
+        "age": 25,
+        "gender": "male",
+        "skill": "run"
+    },
+    {
+        "_id": "593a689e7b5b03f41ce1fd64",
+        "index": 1,
+        "name": "Herman",
+        "surename": "Quinn",
+        "married": true,
+        "age": 24,
+        "gender": "male",
+        "skill": "dance"
+    },
+    {
+        "_id": "593a689e2dcc97f4a30a9988",
+        "index": 2,
+        "name": "Benson",
+        "surename": "Sanford",
+        "married": true,
+        "age": 31,
+        "gender": "male",
+        "skill": "dance"
+    },
+    {
+        "_id": "593a689e452fbf8d88fbcf45",
+        "index": 3,
+        "name": "Pauline",
+        "surename": "Scott",
+        "married": true,
+        "age": 28,
+        "gender": "female",
+        "skill": "dance"
+    },
+    {
+        "_id": "593a689e0e213194070067a4",
+        "index": 4,
+        "name": "Abigail",
+        "surename": "Glenn",
+        "married": false,
+        "age": 24,
+        "gender": "female",
+        "skill": "jump"
+    },
+    {
+        "_id": "593a689e92d447d9c6c3a5f1",
+        "index": 5,
+        "name": "Maxine",
+        "surename": "Gomez",
+        "married": true,
+        "age": 36,
+        "gender": "female",
+        "skill": "run"
+    },
+    {
+        "_id": "593a689e70374e15be566379",
+        "index": 6,
+        "name": "Judy",
+        "surename": "Barnett",
+        "married": true,
+        "age": 33,
+        "gender": "female",
+        "skill": "swim"
+    },
+    {
+        "_id": "593a689edad8e90842f8e46f",
+        "index": 7,
+        "name": "Franks",
+        "surename": "Wall",
+        "married": false,
+        "age": 35,
+        "gender": "male",
+        "skill": "dance"
+    },
+    {
+        "_id": "593a689e0f280c42045e4626",
+        "index": 8,
+        "name": "Krista",
+        "surename": "Burks",
+        "married": true,
+        "age": 24,
+        "gender": "female",
+        "skill": "jump"
+    },
+    {
+        "_id": "593a689e01278a601cf00435",
+        "index": 9,
+        "name": "Elise",
+        "surename": "Frederick",
+        "married": true,
+        "age": 38,
+        "gender": "female",
+        "skill": "dance"
+    },
+    {
+        "_id": "593a689ea46d5c8544be53d3",
+        "index": 10,
+        "name": "Oneill",
+        "surename": "Lee",
+        "married": false,
+        "age": 33,
+        "gender": "male",
+        "skill": "jump"
+    },
+    {
+        "_id": "593a689ee0c128e016d92042",
+        "index": 11,
+        "name": "Bean",
+        "surename": "Dudley",
+        "married": false,
+        "age": 26,
+        "gender": "male",
+        "skill": "swim"
+    },
+    {
+        "_id": "593a689ea14d5756adf659ab",
+        "index": 12,
+        "name": "Nettie",
+        "surename": "Parks",
+        "married": true,
+        "age": 22,
+        "gender": "female",
+        "skill": "swim"
+    },
+    {
+        "_id": "593a689e4f567c62d2b062cf",
+        "index": 13,
+        "name": "Cindy",
+        "surename": "Maddox",
+        "married": true,
+        "age": 25,
+        "gender": "female",
+        "skill": "run"
+    },
+    {
+        "_id": "593a689eb36dfb1ea95dc597",
+        "index": 14,
+        "name": "Dunlap",
+        "surename": "Salas",
+        "married": false,
+        "age": 21,
+        "gender": "male",
+        "skill": "dance"
+    },
+    {
+        "_id": "593a689ee8a9450018633408",
+        "index": 15,
+        "name": "Morgan",
+        "surename": "Carpenter",
+        "married": false,
+        "age": 36,
+        "gender": "female",
+        "skill": "run"
+    },
+    {
+        "_id": "593a689e81f847e22c138d62",
+        "index": 16,
+        "name": "Reyes",
+        "surename": "Landry",
+        "married": false,
+        "age": 35,
+        "gender": "male",
+        "skill": "run"
+    },
+    {
+        "_id": "593a689e918e82808290732c",
+        "index": 17,
+        "name": "Bond",
+        "surename": "Robles",
+        "married": true,
+        "age": 26,
+        "gender": "male",
+        "skill": "run"
+    },
+    {
+        "_id": "593a689e81452860264eae88",
+        "index": 18,
+        "name": "Duke",
+        "surename": "Meyers",
+        "married": false,
+        "age": 37,
+        "gender": "male",
+        "skill": "run"
+    },
+    {
+        "_id": "593a689ed49995c2baaa5ea1",
+        "index": 19,
+        "name": "Vang",
+        "surename": "Moon",
+        "married": false,
+        "age": 21,
+        "gender": "male",
+        "skill": "run"
+    },
+    {
+        "_id": "593a689ebf29a856ce780984",
+        "index": 20,
+        "name": "Figueroa",
+        "surename": "Goodwin",
+        "married": true,
+        "age": 23,
+        "gender": "male",
+        "skill": "dance"
+    },
+    {
+        "_id": "593a689e201514d1eec95d7e",
+        "index": 21,
+        "name": "Jami",
+        "surename": "Rose",
+        "married": false,
+        "age": 24,
+        "gender": "female",
+        "skill": "swim"
+    },
+    {
+        "_id": "593a689e68d346f541d18b64",
+        "index": 22,
+        "name": "Sybil",
+        "surename": "Byers",
+        "married": true,
+        "age": 36,
+        "gender": "female",
+        "skill": "jump"
+    },
+    {
+        "_id": "593a689ed685454c5259d8fa",
+        "index": 23,
+        "name": "Kitty",
+        "surename": "Peters",
+        "married": false,
+        "age": 23,
+        "gender": "female",
+        "skill": "swim"
+    },
+    {
+        "_id": "593a689e6405453d635dc4f4",
+        "index": 24,
+        "name": "Hensley",
+        "surename": "Stevens",
+        "married": false,
+        "age": 25,
+        "gender": "male",
+        "skill": "run"
+    },
+    {
+        "_id": "593a689e576659baa596499a",
+        "index": 25,
+        "name": "Ester",
+        "surename": "Hawkins",
+        "married": true,
+        "age": 33,
+        "gender": "female",
+        "skill": "swim"
+    },
+    {
+        "_id": "593a689e1cc5752a0cf7bab5",
+        "index": 26,
+        "name": "Hattie",
+        "surename": "Downs",
+        "married": true,
+        "age": 26,
+        "gender": "female",
+        "skill": "dance"
+    },
+    {
+        "_id": "593a689ebc71b87ed3f1f083",
+        "index": 27,
+        "name": "Evelyn",
+        "surename": "Hood",
+        "married": true,
+        "age": 37,
+        "gender": "female",
+        "skill": "dance"
+    },
+    {
+        "_id": "593a689e0338922af0ac1a9d",
+        "index": 28,
+        "name": "Lorene",
+        "surename": "Emerson",
+        "married": true,
+        "age": 28,
+        "gender": "female",
+        "skill": "swim"
+    },
+    {
+        "_id": "593a689e14390c644a378b8a",
+        "index": 29,
+        "name": "Blankenship",
+        "surename": "Caldwell",
+        "married": true,
+        "age": 36,
+        "gender": "male",
+        "skill": "run"
+    }
+]
+
+export personArr;
